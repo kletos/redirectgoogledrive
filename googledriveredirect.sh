7335b63f7c6f3fc2ae4330899bf3afed7ea7df9c
kletos <seancourtney@cpu1711.local>
2017-04-28 15:32:18 -0600

    update $HOME path

--- googledriveredirect.sh
+++ googledriveredirect.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 #  googledriveredirect.sh
-#  
+#
 #
 #  Created by Sean Courtney on 4/19/17.
 #
-googleDrivePath=~/Google\ Drive
+googleDrivePath=$HOME/Google\ Drive
 
 echo "Google Drive path found at '$googleDrivePath'"
 
 ### Remove default Desktop and Documents
-rm ~/Documents
-rm ~/Desktop
+rm -r $HOME/Documents
+rm -r $HOME/Desktop
 ### Sync up documents with Google Drive
-ln -s "googleDrivePath"/Documents ~/Documents
-ln -s "googleDrivePath"/Desktop ~/Desktop
-
+ln -s "googleDrivePath"/Documents $HOME/Documents
+ln -s "googleDrivePath"/Desktop $HOME/Desktop
